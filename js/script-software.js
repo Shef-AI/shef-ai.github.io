7781d7d5488ac37f6bc9f09574670a6565bf101d
Sina Tabakhi <stabakhi1@sheffield.ac.uk>
2022-03-11 00:32:01 +0000

    Changed the software page to read data from a JSON file

---
+++ js/script-software.js
@@ -0,0 +1,27 @@
+import data from "../json/software.json" assert { type: "json" };
+
+data.content.forEach(function(element){
+
+  var languages = element["languages"].split(",");
+  var language_list = '<ul>';
+  for (let i = 0; i < languages.length; i++) {
+    language_list += '<li><i class="tf-ion-ios-arrow-forward"></i>' + languages[i] + '</li>';
+  }
+  language_list += '</ul>'
+
+  $('#software-section').append(
+    '<div class="col-md-4 col-sm-6 col-xs-12">' +
+      '<div class="pricing-item">' +
+        '<div class="price-title">' +
+          '<strong class="value">' + element["title"] + '</strong>' +
+          '<p>' + element["description"] + '</p>' +
+        '</div>' +
+
+        '<h3>Languages</h3>' +
+        language_list+
+
+        '<a class="btn btn-main" href="#" target="_blank">MORE</a>' +
+      '</div>' +
+    '</div>' 
+    );
+});
