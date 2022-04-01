b8714f2453eb253d7829b92ff0b376fbce5b798e
Sina Tabakhi <stabakhi1@sheffield.ac.uk>
2022-04-01 10:46:53 +0100

    Added the latest news to the homepage

---
+++ js/script-latest-news.js
@@ -0,0 +1,19 @@
+import data from "../json/news.json" assert { type: "json" };
+
+for (var i = 0; i < data.content.length && i < 3; i++) {
+    var newsElement = data.content[i];
+    $('#latest-news-section').append(
+        '<article class="col-lg-4 col-md-6">' + 
+            '<div class="post-item">' +
+                '<div class="media-wrapper">' +
+                    '<img loading="lazy" src="images/news/' + newsElement["image"] + '" alt="News-' + newsElement["title"] + '" class="img-fluid">' +
+                '</div>' +
+                '<div class="content">' +
+                    '<h3><a href="single-news.html?id=' + newsElement["id"] + '">' + newsElement["title"] + '</a></h3>' +
+                    '<p>' + newsElement["body"] + '</p>' +
+                    '<a class="btn btn-main" href="single-news.html?id=' + newsElement["id"] + '">Read more</a>' +
+                '</div>' +
+            '</div>' +
+        '</article>'
+    );
+}
