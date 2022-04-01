ba17e12cf18d1b9ac64324702af3fcb5aad5a7aa
Sina Tabakhi <stabakhi1@sheffield.ac.uk>
2022-04-01 11:51:50 +0100

    Added the latest events to the homepage

---
+++ js/script-latest-events.js
@@ -0,0 +1,22 @@
+import data from "../json/events.json" assert { type: "json" };
+
+for (var i = 0; i < data.content.length && i < 3; i++) {
+    var eventElement = data.content[i];
+    $('#latest-events-section').append(
+      '<div class="col-md-6 col-sm-4">' +
+        '<img loading="lazy" src="images/events/' + eventElement["image"] + '" alt="Event-' + eventElement["title"] + '" class="img-fluid">' +
+        '<p>' +
+            '<ul class="list-inline">' +
+                '<i class="bi bi-calendar"> ' + eventElement["date"] + '&nbsp;&nbsp;&nbsp;</i>' +
+                '<i class="bi bi-clock"> ' + eventElement["time"] + '</i>' +
+            '</ul>' +
+            '<ul class="list-inline">' +
+                '<i class="bi bi-geo-alt"> ' + eventElement["location"] + '</i>' +
+            '</ul>' +
+        '</p>' +
+        '<p><h3><a href="single-event.html?id=' + eventElement["id"] + '" style="color: black">' + eventElement["title"] + '</a></h3></p>' +
+        '<p>' + eventElement["body"] + '</p>' +
+        '<a class="btn btn-main" href="single-event.html?id=' + eventElement["id"] + '">Read more</a>' +
+      '</div>' 
+    );
+}
