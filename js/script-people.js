6e77357406c2ce873a9acc3f385e7241c713d38b
Sina Tabakhi <stabakhi1@sheffield.ac.uk>
2022-04-16 22:47:18 +0100

    Updated the style of the people page

--- js/script-people.js
+++ js/script-people.js
@@ -2,26 +2,25 @@ import data from "../json/people.json" assert { type: "json" };
 
 data.forEach(function(element){
   $('#people-section').append(
-    '<div class="col-md-4 col-sm-6">' +
-      '<div class="team-member text-center">' +
-        '<div class="member-photo">' +
+    '<div class="col-md-3 col-sm-6 col-xs-6 filtr-item" data-category="mix, ' + element["areas"] + '">' +
+      '<div class="team-member">'+
+        '<div class="portfolio-block member-photo">' +
           '<img class="img-fluid" src="images/people/' + element["image"] + '" alt="Academic-Staff-' + element["name"] + '">' +
-          '<div class="mask">' +
+          '<div class="caption mask">' +
             '<ul class="clearfix">' +
-              '<li><a href="#"><i class="tf-ion-social-facebook"></i></a></li>' +
-              '<li><a href="#"><i class="tf-ion-social-twitter"></i></a></li>' +
-              '<li><a href="#"><i class="tf-ion-social-google-outline"></i></a></li>' +
-              '<li><a href="#"><i class="tf-ion-social-dribbble"></i></a></li>' +
+              '<li><a href="' + element["homepage"] + '" target="_blank"><i class="tf-ion-link"></i></a></li>' +
+              '<li><a href="' + element["linkedin"] + '" target="_blank"><i class="tf-ion-social-linkedin"></i></a></li>' +
+              '<li><a href="' + element["scholar"] + '" target="_blank"><i class="tf-ion-social-google"></i></a></li>' +
+              '<li><a href="' + element["github"] + '" target="_blank"><i class="tf-ion-social-github"></i></a></li>' +
             '</ul>' +
           '</div>' +
-        '</div>' + 
-
-        '<div class="member-content">' +
+        '</div>' +
+        '<div class="member-content text-center">' +
           '<h3>' + element["name"] + '</h3>' +
           '<span>' + element["rank"] + '</span>' +
           '<p>' + element["bio"]  + '</p>' +
         '</div>' +
       '</div>' +
-    '</div>' 
+    '</div>'
     );
 });
