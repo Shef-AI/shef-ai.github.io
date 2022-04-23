65a9134c1bc62aea44bc6faf8eec461656407079
Sina Tabakhi <stabakhi1@sheffield.ac.uk>
2022-04-23 19:26:32 +0100

    Added collapse attribute to people page

--- js/script-people.js
+++ js/script-people.js
@@ -2,10 +2,10 @@ import data from "../json/people.json" assert { type: "json" };
 
 data.forEach(function(element){
   $('#people-section').append(
-    '<div class="col-md-3 col-sm-6 col-xs-6 filtr-item" data-category="mix, ' + element["areas"] + '">' +
+    '<div class="teamblock col-md-3 col-sm-6 col-xs-6 filtr-item" data-category="mix, ' + element["areas"] + '">' +
       '<div class="team-member">'+
         '<div class="portfolio-block member-photo">' +
-          '<img class="img-fluid" src="images/people/' + element["image"] + '" alt="Academic-Staff-' + element["name"] + '">' +
+          '<div style="text-align: center;"><img class="img-fluid" src="images/people/' + element["image"] + '" alt="Academic-Staff-' + element["name"] + '"></div>' +
           '<div class="caption mask">' +
             '<ul class="clearfix">' +
               '<li><a href="' + element["homepage"] + '" target="_blank"><i class="tf-ion-link"></i></a></li>' +
@@ -16,9 +16,13 @@ data.forEach(function(element){
           '</div>' +
         '</div>' +
         '<div class="member-content text-center">' +
-          '<h3>' + element["name"] + '</h3>' +
+          '<a data-toggle="collapse" data-target="#col' + element["id"] + '">' +
+            '<h3>' + element["name"] + ' <span class="bi bi-plus-square" style="font-size: 1rem"></span></h3>' +
+          '</a>' +
           '<span>' + element["rank"] + '</span>' +
-          '<p>' + element["bio"]  + '</p>' +
+          '<div id="col' + element["id"]  + '" class="collapse" data-parent="#accordion">' +
+            '<div class="card-body">' + element["bio"] + '</div>' +
+          '</div>' +
         '</div>' +
       '</div>' +
     '</div>'
