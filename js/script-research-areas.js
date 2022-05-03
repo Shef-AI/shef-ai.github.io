484ce7cb02430b99aa66b8398d5c898a8b43bbd8
Sina Tabakhi <stabakhi1@sheffield.ac.uk>
2022-05-03 16:18:16 +0100

    Changed import assertions to fetch in javascript files to read research Json files

--- js/script-research-areas.js
+++ js/script-research-areas.js
@@ -1,11 +1,13 @@
-import data from "../json/research.json" assert { type: "json" };
-
-data.forEach(function(element){
-  $('#research-section').append(
-      '<div class="col-md-6 col-sm-4">' +
-        '<a href="#"><img src="images/research/' + element["image"] + '" alt="Reseach-Group-' + element["title"] + '" class="img-fluid"></a>' +
-        '<p><h3>' + element["title"] + '</h3></p>' +
-        '<p>' + element["description"] + '</p>' +
-      '</div>' 
-    );
-});
+fetch('../json/research.json')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(function(element){
+      $('#research-section').append(
+        '<div class="col-md-6 col-sm-4">' +
+          '<a href="#"><img src="images/research/' + element["image"] + '" alt="Reseach-Group-' + element["title"] + '" class="img-fluid"></a>' +
+          '<p><h3>' + element["title"] + '</h3></p>' +
+          '<p>' + element["description"] + '</p>' +
+        '</div>' 
+      );
+    });
+  });
