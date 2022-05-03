8b6cc591d6740b5a1f6bd7d02e3406a801146ee8
Sina Tabakhi <stabakhi1@sheffield.ac.uk>
2022-05-03 15:35:02 +0100

    Changed import assertions to fetch in javascript files to read Json files

--- js/script-software.js
+++ js/script-software.js
@@ -1,27 +1,29 @@
-import data from "../json/software.json" assert { type: "json" };
+fetch('../json/software.json')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(function(element){
 
-data.forEach(function(element){
-
-  var languages = element["languages"].split(",");
-  var language_list = '<ul>';
-  for (let i = 0; i < languages.length; i++) {
-    language_list += '<li><i class="tf-ion-ios-arrow-forward"></i>' + languages[i] + '</li>';
-  }
-  language_list += '</ul>'
-
-  $('#software-section').append(
-    '<div class="col-md-4 col-sm-6 col-xs-12">' +
-      '<div class="pricing-item">' +
-        '<div class="price-title">' +
-          '<strong class="value">' + element["title"] + '</strong>' +
-          '<p>' + element["description"] + '</p>' +
-        '</div>' +
-
-        '<h6>Languages</h6>' +
-        language_list+
-
-        '<a class="btn btn-main" href="' + element["link"] + '" target="_blank">MORE</a>' +
-      '</div>' +
-    '</div>' 
-    );
-});
+      var languages = element["languages"].split(",");
+      var language_list = '<ul>';
+      for (let i = 0; i < languages.length; i++) {
+        language_list += '<li><i class="tf-ion-ios-arrow-forward"></i>' + languages[i] + '</li>';
+      }
+      language_list += '</ul>'
+    
+      $('#software-section').append(
+        '<div class="col-md-4 col-sm-6 col-xs-12">' +
+          '<div class="pricing-item">' +
+            '<div class="price-title">' +
+              '<strong class="value">' + element["title"] + '</strong>' +
+              '<p>' + element["description"] + '</p>' +
+            '</div>' +
+    
+            '<h6>Languages</h6>' +
+            language_list+
+    
+            '<a class="btn btn-main" href="' + element["link"] + '" target="_blank">MORE</a>' +
+          '</div>' +
+        '</div>' 
+        );
+    });
+  });
