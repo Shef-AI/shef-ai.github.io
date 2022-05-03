3cc5a6fef826fc756d35bda019df10e9b416d3d2
Sina Tabakhi <stabakhi1@sheffield.ac.uk>
2022-05-03 16:16:51 +0100

    Changed import assertions to fetch in javascript files to read news Json files

--- js/script-latest-news.js
+++ js/script-latest-news.js
@@ -1,20 +1,22 @@
-import data from "../json/news.json" assert { type: "json" };
-
-for (var i = 0; i < data.length && i < 3; i++) {
-    var newsElement = data[i];
-    $('#latest-news-section').append(
-        '<article class="col-lg-4 col-md-6">' + 
-            '<div class="post-item">' +
-                '<div class="media-wrapper">' +
-                    '<img loading="lazy" src="images/news/' + newsElement["image"] + '" alt="News-' + newsElement["title"] + '" class="img-fluid">' +
+fetch('../json/news.json')
+  .then(response => response.json())
+  .then(data => {
+    for (var i = 0; i < data.length && i < 3; i++) {
+        var newsElement = data[i];
+        $('#latest-news-section').append(
+            '<article class="col-lg-4 col-md-6">' + 
+                '<div class="post-item">' +
+                    '<div class="media-wrapper">' +
+                        '<img loading="lazy" src="images/news/' + newsElement["image"] + '" alt="News-' + newsElement["title"] + '" class="img-fluid">' +
+                    '</div>' +
+                    '<div class="content">' +
+                        '<p><i class="bi bi-calendar"> ' + newsElement["date"] +'</i></p>'+
+                        '<h3><a href="single-news.html?id=' + newsElement["id"] + '">' + newsElement["title"] + '</a></h3>' +
+                        '<p>' + newsElement["summary"] + '</p>' +
+                        '<a class="btn btn-main" href="single-news.html?id=' + newsElement["id"] + '">Read more</a>' +
+                    '</div>' +
                 '</div>' +
-                '<div class="content">' +
-                    '<p><i class="bi bi-calendar"> ' + newsElement["date"] +'</i></p>'+
-                    '<h3><a href="single-news.html?id=' + newsElement["id"] + '">' + newsElement["title"] + '</a></h3>' +
-                    '<p>' + newsElement["summary"] + '</p>' +
-                    '<a class="btn btn-main" href="single-news.html?id=' + newsElement["id"] + '">Read more</a>' +
-                '</div>' +
-            '</div>' +
-        '</article>'
-    );
-}
+            '</article>'
+        );
+    }
+  });
--- js/script-news.js
+++ js/script-news.js
@@ -1,18 +1,20 @@
-import data from "../json/news.json" assert { type: "json" };
-
-data.forEach(function(element){
-  $('#news-section').append(
-    '<article class="col-md-4 col-sm-6 col-xs-12 ">'+
-      '<div class="post-item">' + 
-        '<div class="media-wrapper">' + 
-          '<img src="images/news/' + element["image"] +'" alt="News-' + element["title"] + '" class="img-fluid">' + 
-        '</div>' +
-        '<div class="content">' +
-          '<p><i class="bi bi-calendar"> ' + element["date"] +'</i></p>'+
-          '<h3><a href=single-news.html?id=' + element["id"] + '>' + element["title"] + '</a></h3>' +
-          '<p>' + element["summary"] + '</p>' + 
-          '<a class="btn btn-main" href=single-news.html?id=' + element["id"] + '>Read more</a>' + 
-        '</div>' +
-      '</div>' +  
-    '</article>');
-});
+fetch('../json/news.json')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(function(element){
+      $('#news-section').append(
+        '<article class="col-md-4 col-sm-6 col-xs-12 ">'+
+          '<div class="post-item">' + 
+            '<div class="media-wrapper">' + 
+              '<img src="images/news/' + element["image"] +'" alt="News-' + element["title"] + '" class="img-fluid">' + 
+            '</div>' +
+            '<div class="content">' +
+              '<p><i class="bi bi-calendar"> ' + element["date"] +'</i></p>'+
+              '<h3><a href=single-news.html?id=' + element["id"] + '>' + element["title"] + '</a></h3>' +
+              '<p>' + element["summary"] + '</p>' + 
+              '<a class="btn btn-main" href=single-news.html?id=' + element["id"] + '>Read more</a>' + 
+            '</div>' +
+          '</div>' +  
+        '</article>');
+    });
+  });
